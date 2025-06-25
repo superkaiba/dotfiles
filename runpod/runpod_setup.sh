202bf70a232b5b360281eec7b74425c1b4fc5b97
Thomas Jiralerspong <48162623+superkaiba@users.noreply.github.com>
2025-06-25 00:41:20 -0400

    Update runpod_setup.sh

--- runpod/runpod_setup.sh
+++ runpod/runpod_setup.sh
@@ -6,12 +6,9 @@ sudo apt-get install -y less nano htop ncdu nvtop lsof rsync btop jq
 
 # 2) Setup virtual environment
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
 uv python install 3.11
-uv venv
 source .venv/bin/activate
 uv pip install ipykernel simple-gpu-scheduler # very useful on runpod with multi-GPUs https://pypi.org/project/simple-gpu-scheduler/
-python -m ipykernel install --user --name=venv # so it shows up in jupyter notebooks within vscode
 
 # 3) Setup dotfiles and ZSH
 mkdir git && cd git
